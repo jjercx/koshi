5570a8f8fbf7517bb3db47b204a077e433e8059f
Alexandra <Alexandra>
2016-04-05 19:04:50 -0500

    I modified character and item classes and create MainCharacter class :v

--- src/koshi/Item.java
+++ src/koshi/Item.java
@@ -10,5 +10,87 @@ package koshi;
  * @author Alexandra Espichán Linares
  */
 public class Item {
+    private int tipo;
+    private int x;
+    private int y;
+    private int height;
+    private int weight;
+
+    public Item(int tipo, int x, int y, int height, int weight){
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.weight = weight;
+        this.tipo = tipo;
+    }
     
+    /**
+     * @return the tipo
+     */
+    public int getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+     * @return the weight
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
