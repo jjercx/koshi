5791f14b8e1e57864656da8fba0f4feeecc8d5c4
Juan José Ramírez Calderón <jjramirez@pucp.pe>
2016-04-06 01:05:28 -0500

    add cell and map

--- src/koshi/Cell.java
+++ src/koshi/Cell.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package koshi;
 
 /**
@@ -10,5 +5,90 @@ package koshi;
  * @author Juan José Ramírez Calderón
  */
 public class Cell {
+    
+    private Position pos;
+    private Position next[];
+    
+    private boolean walkable;
+    private String type; //trigger, action, duo, normal
+    private char sprite; 
+
+    public Cell(boolean walkable, String type, char sprite, int i, int j) {
+        this.walkable = walkable;
+        this.type = type;
+        this.sprite = sprite;
+        pos = new Position(i,j);
+    }
+
+    /**
+     * @return the pos
+     */
+    public Position getPos() {
+        return pos;
+    }
+
+    /**
+     * @param pos the pos to set
+     */
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
+
+    /**
+     * @return the next
+     */
+    public Position[] getNext() {
+        return next;
+    }
+
+    /**
+     * @param next the next to set
+     */
+    public void setNext(Position[] next) {
+        this.next = next;
+    }
+
+    /**
+     * @return the walkable
+     */
+    public boolean isWalkable() {
+        return walkable;
+    }
+
+    /**
+     * @param walkable the walkable to set
+     */
+    public void setWalkable(boolean walkable) {
+        this.walkable = walkable;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the sprite
+     */
+    public char getSprite() {
+        return sprite;
+    }
+
+    /**
+     * @param sprite the sprite to set
+     */
+    public void setSprite(char sprite) {
+        this.sprite = sprite;
+    }
+    
     
 }
--- src/koshi/Map.java
+++ src/koshi/Map.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package koshi;
 
 /**
@@ -11,4 +6,103 @@ package koshi;
  */
 public class Map {
     
+    private int width;
+    private int height;
+    private Cell matrix[][];
+    
+    private String commandA;
+    private String commandB;
+    private String commandD;
+    
+    Map(int width, int height){
+        this.width = width;
+        this.height = height;
+        matrix = new Cell[width][height];
+    }
+
+    
+    /**
+     * @return the width
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+     * @return the height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+     * @return the matrix
+     */
+    public Cell[][] getMatrix() {
+        return matrix;
+    }
+
+    /**
+     * @param matrix the matrix to set
+     */
+    public void setMatrix(Cell[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    /**
+     * @return the commandA
+     */
+    public String getCommandA() {
+        return commandA;
+    }
+
+    /**
+     * @param commandA the commandA to set
+     */
+    public void setCommandA(String commandA) {
+        this.commandA = commandA;
+    }
+
+    /**
+     * @return the commandB
+     */
+    public String getCommandB() {
+        return commandB;
+    }
+
+    /**
+     * @param commandB the commandB to set
+     */
+    public void setCommandB(String commandB) {
+        this.commandB = commandB;
+    }
+
+    /**
+     * @return the commandD
+     */
+    public String getCommandD() {
+        return commandD;
+    }
+
+    /**
+     * @param commandD the commandD to set
+     */
+    public void setCommandD(String commandD) {
+        this.commandD = commandD;
+    }
+
 }
