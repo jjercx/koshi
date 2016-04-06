b2c53197935d089aa85765ae72e22fc2ee10100d
MAURICIO <MAURICIO>
2016-04-05 20:32:20 -0500

    The verifyInputKey has been improved but not finished

--- src/koshi/CommandInterpreter.java
+++ src/koshi/CommandInterpreter.java
@@ -11,93 +11,38 @@ import java.util.Scanner;
  */
 public class CommandInterpreter {
     
-    //This attribute is the option for the main menu.
-    private int option;
-    //This attribute is for the command secuency in game.
-    private String secuency;
-    //This attribute is for the current key pressed to do an action.
-    private char gameKey;
+    
 
     public void CommandInterpreter(){
         
     }
-    public int verifyMainMenuInput(){
+    
+    
+    
+    
+    public int verifyMainMenuInput(int op){
         /*This method takes the main menu's option and verify it
           to do an specific event.*/
-        int op;
-        Scanner read = new Scanner(System.in);
-        op = read.nextInt();
-        
-        if (op == 1) {
+        while(true){
             //When the player chose this option, the game its supposed to start.
-            System.out.println("Empieza nueva partida\n");
-            return 1;
-        }
-        else if (op == 2) System.exit(0);
-        else {
-            //When the CI detects an unknown character, it will request to the player
-            //to retype the option character.
-            System.out.println("Por favor ingrese una opción válida.\n");
-        }
-        return 0;
+            if (op == 1) break;
+            else if (op == 2) System.exit(0);
+            else {
+                /*When the CI detects an unknown character, it will request to the player
+                  to retype the option character.*/
+                System.out.println("Por favor ingrese una opción válida.\n");
+            }
+        }        
+        return 1;
     }
-    public int verifyInputKey(int eventFlag){
-        String key;
-        Scanner readKey = new Scanner(System.in);
-        key = readKey.nextLine();
-        if (key.length() == 1){
-            /*In this part could be any of the movement or special keys or the ESC key*/
-        }else{
-            /*This is the command secuency part, when the CI verifies if that secuency
-              is correct.*/
+    public int verifyInputKey(String command, int type){
+        if (command.equals("ESC")) System.exit(0);
+        if (type == 0){
+            /*This means that we're refering at player's movement*/
+            
         }
         return 0;
 
     }
-    
-    /**
-     * @return the option
-     */
-    public int getOption() {
-        return option;
-    }
-
-    /**
-     * @param option the option to set
-     */
-    public void setOption(int option) {
-        this.option = option;
-    }
-
-    /**
-     * @return the secuency
-     */
-    public String getSecuency() {
-        return secuency;
-    }
-
-    /**
-     * @param secuency the secuency to set
-     */
-    public void setSecuency(String secuency) {
-        this.secuency = secuency;
-    }
-
-    /**
-     * @return the gameKey
-     */
-    public char getGameKey() {
-        return gameKey;
-    }
-
-    /**
-     * @param gameKey the gameKey to set
-     */
-    public void setGameKey(char gameKey) {
-        this.gameKey = gameKey;
-    }
-    
-    
-    
     
 }
